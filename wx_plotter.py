32110e016db514950ce75f257a5170d86681143e
mtnesbitt <stmtn01@moravian.edu>
2017-08-03 08:32:42 -0400

    Live traffic data from tzu and correct locking practices with the queue with cosmetic changes.

--- wx_plotter.py
+++ wx_plotter.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python
 import datetime
-from threading import Thread
-
-import matplotlib
-
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
-from matplotlib.backends.backend_wx import NavigationToolbar2Wx
 from matplotlib.figure import Figure
 from attack_info_manager import AttackInfoManager
-import sys
-
 from mpl_toolkits.basemap import Basemap
-
 from wx import *
-
-
-class Map(Frame):
+import wx
+class TrafficMap(Frame):
     def __init__(self):
         Frame.__init__(self, None, -1,
-                       'CanvasFrame', size=(600, 600), pos=(10,10))
+                       'Moravian Traffic Project', size=(600, 600), pos=(10,10))
 
-        self.SetBackgroundColour("Gray")
+        self.SetBackgroundColour("white")
 
         panel = Panel(self, size=(1000, 1000), pos=(650, 0))
 
-        text = StaticText(panel, -1, style=ALIGN_LEFT, pos=(10, 10))
+        text = StaticText(panel, -1, style=ALIGN_RIGHT, pos=(10, 10))
 
         text.SetLabel("Everyday, thousands of users attempt to get past the Moravian firewall and fail "
                       "to do so."
@@ -34,8 +25,20 @@ class Map(Frame):
                             "The color of the dots corresponds to the type of protocol that is being used. "
                             "Red is for tcp, blue is udp, green is icmp, and cyan is gre")
         text.Wrap(600)
+        font = wx.Font('70')
+        text.SetFont(font)
 
         text.SetBackgroundColour("WHITE")
+
+        image_panel = Panel(self, pos=(150, 475), size=(1000, 240))
+        image_panel.SetBackgroundColour('white')
+
+        img1 = Image("diagram.png", BITMAP_TYPE_ANY)
+        w = img1.GetWidth()
+        h = img1.GetHeight()
+
+        img2 = img1.Scale(w / 1.7, h / 2)
+        sb2 = StaticBitmap(image_panel, -1, BitmapFromImage(img2), pos=(0, 0))
 
         self.figure = Figure()
         self.canvas = FigureCanvas(self, -1, self.figure)
@@ -65,7 +68,7 @@ class Map(Frame):
 
     def callback(self, event):
         if(len(self.data.get_attack_times()) == 0):
-            timestamp = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(seconds=5),
+            timestamp = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(seconds=1),
                                                    '%Y/%m/%d%H:%M:%S')
         else:
             timestamp = self.data.get_attack_times()[-1]
@@ -81,7 +84,7 @@ class Map(Frame):
 class App(App):
     def OnInit(self):
         'Create the main window and insert the custom frame'
-        frame = Map()
+        frame = TrafficMap()
         frame.Show(True)
         return True
 app = App(0)
